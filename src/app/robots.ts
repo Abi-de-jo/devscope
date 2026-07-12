41a9c2d720b1bb10b726c4bd509ea1b70b998b27
Abi-de-jo <abisheikabisheik102@gmail.com>
2026-07-12 06:43:25 +0530

    feat: add robots.txt configuration to restrict crawling of private routes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,41 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const commonDisallows = ["/dashboard", "/api", "/report", "/profile"];
+
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/dashboard", "/api", "/report"],
-    },
-    sitemap: "https://GitRating.mozen.in/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: commonDisallows,
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+        disallow: commonDisallows,
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+        disallow: commonDisallows,
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+        disallow: commonDisallows,
+      },
+      {
+        userAgent: "Google-Extended",
+        allow: "/",
+        disallow: commonDisallows,
+      },
+      {
+        userAgent: "Applebot-Extended",
+        allow: "/",
+        disallow: commonDisallows,
+      },
+    ],
+    sitemap: "https://gitrating.mozen.in/sitemap.xml",
   };
 }
